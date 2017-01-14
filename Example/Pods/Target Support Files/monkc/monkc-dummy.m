c0c7962d207c40947afe4c9d66580355e9552792
Sun YuLi <sunpaq@gmail.com>
2017-01-14 17:02:38 +0800

    fix the file missing warnings

---
+++ Example/Pods/Target Support Files/monkc/monkc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_monkc : NSObject
+@end
+@implementation PodsDummy_monkc
+@end
---
+++ Example/Pods/Target Support Files/monkc/monkc-umbrella.h
@@ -0,0 +1,32 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "MCArray.h"
+#import "MCArrayLinkedList.h"
+#import "MCBits.h"
+#import "MCBuffer.h"
+#import "MCClock.h"
+#import "MCContext.h"
+#import "MCException.h"
+#import "MCGeometry.h"
+#import "MCGraph.h"
+#import "MCHeap.h"
+#import "MCIO.h"
+#import "MCJNI.h"
+#import "MCLexer.h"
+#import "MCLinkedList.h"
+#import "MCMath.h"
+#import "MCProcess.h"
+#import "MCSet.h"
+#import "MCSocket.h"
+#import "MCSort.h"
+#import "MCString.h"
+#import "MCThread.h"
+#import "MCTree.h"
+#import "MCUnitTest.h"
+#import "monkc.h"
+
+FOUNDATION_EXPORT double monkcVersionNumber;
+FOUNDATION_EXPORT const unsigned char monkcVersionString[];
+
